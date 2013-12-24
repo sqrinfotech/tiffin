649b50d41ba5d038fb6c8e277cdeffaf985b912b
raeesaa <raeesaa.rm@gmail.com>
2013-12-24 17:27:14 +0530

    Added validations, not tested

--- routes/user.js
+++ routes/user.js
@@ -28,7 +28,8 @@ var UserSchema = new Schema({
   username: {
     type: String,
     unique: true,
-    required: true
+    required: true,
+    validate: usernameValidator
   },
   salt: {
     type: String,
@@ -38,13 +39,20 @@ var UserSchema = new Schema({
     type: String,
     required: true
   },
-  name: String,
+  name: {
+    type: String,
+    validate: nameValidator
+  },
   email: {
     type: String,
     unique: true,
-    required: true
+    required: true,
+    validate: emailValidator
   },
-  address: String,
+  address: {
+    type: String,
+    validate: addressValidator
+  },
   location: String,
   loginIps: Array,
   confirmationToken:  String,
@@ -59,6 +67,42 @@ var UserSchema = new Schema({
   createdAt: Date,
   updatedAt: Date
 });
+
+//Validations added by Raeesaa
+
+var nameValidator = validate[validate('notNull'), validate('isAlpha')];
+
+var emailValidator = validate[validate('notNull'), validate('isEmail')];
+
+var usernameValidator = validate[validate('notNull'), 
+                        validate({message: 'Username should be between 2 and 20 characters'}, 'len', 2, 20),
+                        validate({message: 'Username must contain only aplhanumeric characters or underscores'}, 'regex', /^[a-zA-Z0-9_]]*$/)];
+
+var passwordValidator = validate[validate('notNull'), validate({message: "Password should be minimum 8 characters long"}, 'len', 8)];
+
+var addressValidator = validate[validate('notNull')];
+
+var zipCodeValidator = validate[validate('notNull'), validate('isNumeric')];
+
+/*UserSchema.path('name').validate(isEmpty(val), 'Name is required');
+
+UserSchema.path('username').validate(isEmpty(val), 'Username is required');
+
+UserSchema.path('password').validate(isEmpty(val), 'Password is required');
+
+UserSchema.path('email').validate(isEmpty(val), 'Email ID is required');
+
+UserSchema.path('address').validate(isEmpty(val), 'Address is required');
+
+UserSchema.path('zipcode').validate(isEmpty(val), 'ZipCode is required');
+
+
+UserSchema.path('username').validate(message: "Username should be between 2 and 20 characters"}, 'len', 2, 20);
+
+UserSchema.path('username').validate(/^[a-zA-Z0-9_]]*$/
+  , 'Username must contain only aplhanumeric characters or underscores');
+*/
+
 
 var User = mongoose.model('User',UserSchema);
 
@@ -120,3 +164,13 @@ exports.create = function(req, res, next){
 function randomToken () {
   return crypto.randomBytes(48).toString('hex');
 };
+
+/*var isEmpty = function(val){
+  if(val == null){
+    return false;
+  }
+  else{
+    return true;
+  }
+
+};*/
