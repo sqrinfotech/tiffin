c76e252692513d52e4f707beb216226ae7d79ad4
raeesaa <raeesaa.rm@gmail.com>
2014-01-09 17:07:21 +0530

    small modifications

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,8 @@
 exports.home = function(req, res){
   var op=req.body.submitButton;
   if(op=='Yes'){
-    console.log("Yaaaaaaaaaaaaaaaaaaaaaaaaaaa "); 
     res.render('dabbawalas/login'); 
   } else{
-    console.log("Noooooooooooooooooooo ");  
     res.render('users/login'); 
   };
 };
